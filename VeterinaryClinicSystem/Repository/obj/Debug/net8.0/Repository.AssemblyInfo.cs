74bfd100068809028ad102b6107ddb858761f771
Nguyễn Hoàng Long 40 <longnhse172340@fpt.edu.vn>
2025-07-18 13:38:26 +0700

    add OwnerPet in PetList

--- VeterinaryClinicSystem/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ VeterinaryClinicSystem/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5985033b85477ca4fe9deae6da61d38db6d8da69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4431e2f3347a62268c5632fc4c078f3a924802")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
